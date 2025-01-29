b9e9c7b4caa482e6f1b0834e978746546731446b
David Iraheta <david.iraheta.89@gmail.com>
2025-01-29 15:51:15 -0700

    Python301.1.29

--- 04_web-scraping/04_03_poke_info.py
+++ 04_web-scraping/04_03_poke_info.py
@@ -11,4 +11,32 @@
 # `.html` page which allows you to also display the sprites of each Pokémon.
 # Check out the guides they provide: https://pokeapi-how.appspot.com/page5
 
+
+import requests
+
 BASE_URL = "https://pokeapi.co/api/v2/"
+
+golem = requests.get(BASE_URL + "pokemon/golem")
+golem_data = golem.json()
+toxicroak = requests.get(BASE_URL + "pokemon/toxicroak")
+toxicroak_data = toxicroak.json()
+hydreigon = requests.get(BASE_URL + "pokemon/hydreigon")
+hydreigon_data = hydreigon.json()
+houndoom = requests.get(BASE_URL + "pokemon/houndoom")
+houndoom_data = houndoom.json()
+crawdaunt = requests.get(BASE_URL + "pokemon/crawdaunt")
+crawdaunt_data = crawdaunt.json()
+celebi = requests.get(BASE_URL + "pokemon/celebi")
+celebi_data = celebi.json()
+
+
+pokemon = ["Golem", "Toxicroak", "Hydreigon", "Houndoom", "Crawdaunt", "Celebi"]
+
+for poke in pokemon:
+    response = requests.get(BASE_URL + f"pokemon/{poke.lower()}")
+    data = response.json()
+    print(f"{data['name']} ({data['id']})")
+    print("Types:")
+    for type_ in data["types"]:
+        print(f"  - {type_['type']['name']}")
+    print("\n")
