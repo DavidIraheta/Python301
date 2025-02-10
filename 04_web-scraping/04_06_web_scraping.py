91eeb5ebb7e8eac068041f009d9a2968e25487de
David Iraheta <david.iraheta.89@gmail.com>
2025-02-10 13:27:23 -0700

    Python301.2.10

--- 04_web-scraping/04_06_web_scraping.py
+++ 04_web-scraping/04_06_web_scraping.py
@@ -15,16 +15,17 @@ URL = "https://books.toscrape.com/catalogue/category/books/science-fiction_16/in
 response = requests.get(URL)
 soup = BeautifulSoup(response.text, "html.parser")
 books = soup.find_all("article", class_="product_pod")
+book_rating = soup.find_all("p", class_="star-rating")
 book_list = []
 for book in books:
     title = book.h3.a["title"]
     price = book.find("p", class_="price_color").text
     rating = book.p["class"][1]
     book_list.append({"title": title, "price": price, "rating": rating})
-    print(title, price, rating)
+    print(title, price, book_rating)
 
 with open("books.json", "w") as file:
     json.dump(book_list, file)
 
-print()
+
 
