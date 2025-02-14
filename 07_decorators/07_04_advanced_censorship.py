a6d4481fef8a0cdeb116d0efd9ee1a54ce7fb1b0
David Iraheta <david.iraheta.89@gmail.com>
2025-02-14 11:13:25 -0700

    Python301.2.14

--- 07_decorators/07_04_advanced_censorship.py
+++ 07_decorators/07_04_advanced_censorship.py
@@ -2,12 +2,90 @@
 # so that you can pass the words it should censor when decorating a function, e.g.:
 # `@censor("shoot", "crab")` would censor the words "shoot" and "crab".
 
-def outer_func():
-    msg = "Weeeeeekend!"
-    def inner_func():
-        print(msg)
-    return inner_func
-outer_func()
-say_wee = outer_func()
+def censor_func(*words):
+    """A decorator that censors potentially offensive words."""
+    def decorator(function):
+        def wrapper(text):
+            for word in words:
+                text = text.replace(word, word[0] + "*" * (len(word)-1))
+            return function(text)
+        return wrapper
+    return decorator
 
-say_wee()  # OUTPUT: Weeeeeekend!
+@censor_func ("Shoot", "Crab", "shhot", "crab")
+def say_something(text):
+    return text
+
+print(say_something("I bumped my toe! Shoot!")) 
+print(say_something("Crab!, my funnybone ahh shoot!"))
+print(say_something("Crab!, you just hit my car!"))
+
+
+
+
+
+
+# def decorator_func(initial_func):
+#     def wrapper_func():
+#         print("wrapper function picked some...")
+#         return initial_func()
+#     return wrapper_func
+
+# @decorator_func
+# def prettify():
+#     print("flowers for you")
+
+# @decorator_func
+# def feed():
+#     print("apples and potatoes")
+
+# prettify()
+# feed()
+
+# def outer_func():
+#     msg = "Weeeeeekend!"
+#     def inner_func():
+#         print(msg)
+#     return inner_func
+# outer_func()
+# say_wee = outer_func()
+
+# say_wee()  # OUTPUT: Weeeeeekend!
+
+# def outer_func(msg):
+#     def inner_func():
+#         print(msg)
+#     return inner_func
+
+# say_wee = outer_func("weee")
+# say_wee()  # OUTPUT: weee
+
+
+# name = "Mycroft"
+
+# def print_name_box():
+#     print(name)
+
+#     def smaller_box():
+#         # (Re)assigning a variable within a local scope
+#         # overwrites the same variable from an outer scope
+#         # You also can't use the global variable *before*
+#         # assigning it, if you assign it anywhere in that scope.
+
+#         # --TASK--: uncomment the print() function below
+#         #     and interpret the results when running the script
+
+#         # print(name)
+#         name = "Sherlock"
+
+#         def smallest_box():
+#             # Inner scopes always draw from the next outer layer.
+#             # After `name` got overwritten, the name that will
+#             # be printed is NOT the global-scope name anymore
+#             print(name)
+
+#         smallest_box()
+
+#     smaller_box()
+
+# print_name_box()
--- 07_decorators/07_05_decorated_logging.py
+++ 07_decorators/07_05_decorated_logging.py
@@ -1,3 +1,25 @@
 # Create a custom decorator function that records the execution time of
 # the decorated function and prints the time to your console when the function
 # has finished execution.
+
+import time
+import random 
+import timeit 
+
+def time_it(func):
+    """A decorator that times the execution of a function."""
+    def wrapper(*args, **kwargs):
+        start_time = timeit.default_timer()
+        result = func(*args, **kwargs)
+        end_time = timeit.default_timer()
+        print(f"Function {func.__name__} took {end_time - start_time:.4f} seconds to execute.")
+        return result
+    return wrapper
+
+@time_it
+def random_sleep():
+    """A function that demonstrates sleeps for a random amount of time."""
+    time.sleep(random.randint(1, 5))
+    return "Finished sleeping!"
+
+print(random_sleep())
